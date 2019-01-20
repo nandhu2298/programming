af7c9b3b62d69e3c0279ef379f3564a9927832ca
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-01-20 19:35:47 +0530

    palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+   
+   int n,r,s=0,t;
+   scanf("%d",&n);
+   t=n;
+   
+   while(n>0)
+   {
+       
+       r=n%10;
+       s=(s*10)+r;
+       n=n/10;
+       
+}
+ 
+ if(s==t)
+ {
+     printf("yes");
+ }
+ else{
+     
+     
+     printf("no");
+ }
+   
+  }
