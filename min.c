71fb8d15afae4908286395c8e6d481e12290c053
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-01-22 19:49:46 +0530

    min.c

---
+++ min.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+   int n,i,m,a[100000];
+   scanf("%d",&n);
+   
+   for(i=0;i<n;i++)
+   {
+       
+       
+       scanf("%d",&a[i]);
+       
+   }
+   
+   m=a[0];
+    for(i=1;i<n;i++)
+   {
+    
+   if(m>a[i])
+   {
+       
+       m=a[i];
+   }
+    
+}
+
+printf("%d",m);
+
+}
