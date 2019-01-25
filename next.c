8012b20fe538f77cb9f84bca84d7cdb80b3e1029
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-01-25 21:47:57 +0530

    next.c

---
+++ next.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+   
+int N;
+   
+   scanf("%d",&N);
+   
+   if(N<100000000)
+   {
+       
+       
+     
+      printf("%d",N+1);  
+       
+       
+   }
+   
+   
+      
+      
+       return 0;
+   
+}
