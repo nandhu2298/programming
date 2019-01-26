bb634f88a1d1e87cbaec686fda5598acd2c73adc
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-01-26 10:10:05 +0530

    numeric.c

---
+++ numeric.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  
+char S[1000];
+int i,c=0,n;
+
+gets(S);
+n=strlen(S);
+for(i=0;S[i]!='\0';i++)
+{
+    
+    if((S[i]>='0'&&S[i]<='9')||S[i]=='.')
+    {
+        c++;
+        printf("%d %d",c,n);
+    }
+}
+if(c==n)
+printf("Yes");
+else
+printf("No");
+
+return 0;
+}
