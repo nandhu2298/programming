d257b276fc5506889254921f6dc17e1afb53f396
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-01-31 21:13:57 +0530

    nsumm.c

---
+++ nsumm.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int n,i,s=0;
+    scanf("%d",&n);
+    if(n>=1&&n<=100000)
+    {
+        
+        
+     for(i=1;i<=n;i++)   
+        
+        
+        
+        
+        {
+        
+        s=s+i;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    printf("%d",s);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
