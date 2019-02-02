c39a8e8a2d7ea3c91fc4cd9951006db10b274230
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-02 20:25:02 +0530

    cuboid.c

---
+++ cuboid.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+   int L,B,H,a,v;
+   scanf("%d %d %d",&L,&B,&H);
+   
+   a=2*((L*B)+(B*H)+(H*L));
+   v=L*B*H;
+   
+   
+   printf("%d %d",a,v);
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   return 0;
+   
+   
+}
