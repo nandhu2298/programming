689a59ac9516033a872278ecd3994ec99d700477
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-06 05:27:33 +0530

    numericstr.c

---
+++ numericstr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   char s[10000];
+   int i;
+   gets(s);
+   for(i=0;s[i]!='\0';i++)
+   {
+       
+       
+       if(s[i]>='0'&&s[i]<='9')
+       {
+           printf("%c",s[i]);
+       }
+       
+   }
+    return 0;
+}
