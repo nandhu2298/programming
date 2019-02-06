bc6a099283812c7ef37f745eb1365f4a8cef001f
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-06 20:29:06 +0530

    gcd.c

---
+++ gcd.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+int N,M,i,gcd;
+scanf("%d %d",&N,&M);
+
+for(i=1;i<=N&&i<=M;i++)
+{
+    
+    
+    
+    
+    if(N%i==0&&M%i==0)
+    gcd=i;
+    
+    
+    
+}
+
+
+
+
+    printf("%d",gcd);
+    
+    return 0;
+}
