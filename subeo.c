d5359c43c7c9ddcee58c1c69bd76e8946c9fc4f5
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-06 20:03:12 +0530

    subeo.c

---
+++ subeo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ int M,N,s;
+ scanf("%d %d",&M,&N);
+ s=M-N;
+ if(s%2==0)
+  printf("even");
+ else
+  printf("odd");
+      
+      
+      
+    
+    return 0;
+}
