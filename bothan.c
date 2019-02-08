71c47594ba3dd1aadf94fac8c8fa9a26c9b4da53
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-08 11:42:35 +0530

    bothan.c

---
+++ bothan.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+  char s[1000];
+  int i,c=0,d=0;
+  gets(s);
+  
+ for(i=0;s[i]!='\0';i++)
+ {
+     
+     if(s[i]>='a'&&s[i]<='z' || s[i]>='A'&&s[i]<='Z')
+     {
+         
+         c++;
+         
+         
+     }
+     
+     else if(s[i]>='1'&&s[i]<='9' )
+     {
+         
+         d++;
+         
+         
+     }
+ 
+     
+ }
+ 
+ printf("%d %d",c,d);
+ 
+  
+  if(c!=0&&d!=0)
+  printf("Yes");
+  else
+  printf("No");
+  
+  
+  
+    return 0;
+}
