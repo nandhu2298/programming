649624847c4dd82f9f9a063467b4025f0b1aa45a
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-08 07:57:42 +0530

    nearevn.c

---
+++ nearevn.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+int N,i;
+scanf("%d",&N);
+
+
+for(i=N;i>=0;i--)
+{
+    
+    if(i%2==0)
+    {
+        
+        
+        printf("%d",i);
+        break;
+    }
+    
+    
+}
+
+
+
+
+    return 0;
+}
