6e04695f1cd044f7b8bb594fa735299c48790677
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-08 08:29:21 +0530

    prime1.c

---
+++ prime1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+int N,i,f=0;
+scanf("%d",&N);
+for(i=2;i<=N/2;i++)
+{
+    
+    
+    if(N%i==0)
+    {
+        
+        
+        f=1;
+        break;
+    }
+    
+    
+}
+
+if(f==0)
+printf("yes");
+else
+printf("no");
+
+    return 0;
+}
