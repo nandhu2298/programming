16b128705e987dd32cc8cd407058e2ff949cea4e
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-09 21:32:40 +0530

    divmod.c

---
+++ divmod.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    
+ int a,b;
+ char c;
+ 
+ scanf("%d %c %d",&a,&c,&b);
+ if(c=='/')
+  printf("%d",a/b);
+  
+  else
+  
+    
+     printf("%d",a%b);
+    
+}
