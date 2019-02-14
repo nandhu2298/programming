72a4d3841eba212049e27efdadbbb8f3f1de59b0
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-14 06:11:05 +0530

    trim.c

---
+++ trim.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+char s[100000];
+gets(s);
+
+int i;
+for(i=0;s[i]!='\0';i++)
+{
+    if(s[i]==' ')
+    {
+        
+        continue;
+        
+        
+    }
+      
+    
+    printf("%c",s[i]);
+  
+    
+    
+}
+
+
+
+
+
+
+
+
+
+}
