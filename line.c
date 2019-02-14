1525fc6d1abbc3eafb664995e3871c99d1d68300
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-14 05:33:45 +0530

    line.c

---
+++ line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   int x1,y1,x2,y2,x3,y3;
+   scanf("%d %d %d %d %d %d",&x1,&y1,&x2,&y2,&x3,&y3);
+   if(x1==0&&x2==0
+   &&x3==0)
+    printf("yes");
+    
+    else if(y1==0&&y2==0&&y3==0)
+    printf("yes");
+    else
+    printf("no");
+    return 0; 
+    
+    
+    
+
+}
