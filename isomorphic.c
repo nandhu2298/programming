c2e9f68f1b0a054c102351979e64c8f18be1c846
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-15 19:54:34 +0530

    isomorphic.c

---
+++ isomorphic.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+  
+  char a[1000],b[1000];
+  int l1=0,l2=0,i,c=0;
+  gets(a);
+  gets(b);
+  for(i=0;a[i]!='\0';i++)
+  {
+      
+      l1++;
+      
+  }
+    for(i=0;a[i]!='\0';i++)
+  {
+      
+      l2++;
+      
+  }
+  
+  
+  if(l1==l2)
+  {
+      
+      
+     for(i=0;a[i]!='\0';i++)
+     {
+         if(a[i]==a[i+1]&&b[i]==b[i+1])
+         {
+             
+           c++;
+             
+         }
+        
+        
+          if(a[i]!=a[i+1]&&b[i]!=b[i+1])
+         {
+             
+             c++;
+               
+             
+         }
+        
+     }
+      
+      if(c==l1)
+    printf("yes");
+    else
+    printf("no");
+      
+      
+}
+  
+  else
+  {
+      
+      printf("no");
+  }
+
+    return 0;
+}
