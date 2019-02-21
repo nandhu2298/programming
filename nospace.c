adc3b35795623cc2af2b8e288e6c10403f4934ca
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-21 15:33:44 +0530

    nospace.c

---
+++ nospace.c
@@ -0,0 +1,36 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+char s[100000];
+gets(s);
+
+int i;
+for(i=0;s[i]!='\0';i++)
+{
+    if(s[i]==' ')
+    {
+        
+        continue;
+        
+        
+    }
+      
+    
+    printf("%c",s[i]);
+  
+    
+    
+}
+
+
+
+
+
+
+
+
+
+}
