7ffb438b9d632d22cee0a52315453cbe1ebeed65
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-02-23 15:32:49 +0530

    evfac.c

---
+++ evfac.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+int N,i;
+scanf("%d",&N);
+
+for(i=1;i<=N;i++)
+{
+    
+    if(N%i==0&&i%2==0)
+    {
+        
+        
+        printf("%d ",i);
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+  return 0;
+}
