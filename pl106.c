4d36db14b4e873bbce970fab91bb90044fea7f5c
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-03-05 17:23:47 +0530

    pl106.c

---
+++ pl106.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+   int N,a[100000],i,j;
+   scanf("%d",&N);
+   for(i=0;i<N;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   
+      for(i=0;i<N;i++)
+   {
+     for(j=i+1;j<N;j++)
+     {
+         if(a[i]!='\0'&&a[j]!='\0')
+         {
+      if(a[i]==a[j])  
+      {
+          a[j]='\0';
+      }
+         
+     }
+       
+     }
+   }
+   
+   
+      for(i=0;i<N;i++)
+   {
+        if(a[i]!='\0')
+        printf("%d ",a[i]);
+       
+   }
+   
+  
+    return 0;
+}
