d5442fe616f32b0505608eabfd0035a6393516bd
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-03-05 08:14:14 +0530

    pl74.c

---
+++ pl74.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+ int A,B,p=1,i;
+ scanf("%d %d",&A,&B);
+ 
+ 
+ 
+ 
+ for(i=1;i<=B;i++)
+ {
+     p=p*A;
+     }
+     printf("%d",p);
+     
+    return 0;
+}
