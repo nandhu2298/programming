e2d9b424c1a8f16554300ccc104b511171e8c61d
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-03-06 20:49:27 +0530

    pl67.c

---
+++ pl67.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+int N,i,f=1;
+scanf("%d",&N);
+if(N>=1&&N<=25)
+
+{
+    
+    
+    
+    
+    for(i=1;i<=N;i++)
+    {
+        
+        f=f*i;
+    }
+    
+    
+    
+    printf("%d",f);
+    
+}
+
+    return 0;
+}
