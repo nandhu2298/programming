c0bb53b5af83b6d4f2c9cae025acbb7e89cdc6eb
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-03-10 09:13:52 +0530

    pl97.cc

---
+++ pl97.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+int l,r,i,s=0;
+scanf("%d %d",&l,&r);
+for(i=l;i<=r;i++)
+{
+    
+    
+    if(i%2!=0)
+    {
+        
+        s=s+i;
+    }
+    
+}
+  printf("%d",s);
+  
+    return 0;
+}
