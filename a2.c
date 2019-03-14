4f1f6b99a52aa62e231440ba7b3135b35ef0ec97
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-03-14 16:02:40 +0530

    a2.c

---
+++ a2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main()
+{
+    int N,a[100],i,j,k;
+   scanf("%d",&N);
+   for(i=0;i<N;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   
+   
+   for(i=0;i<N;i++)
+   {
+     
+ 
+       for(j=i+1;j<N;j++)
+       {
+         
+       
+         for(k=j+1;k<N;k++)
+         {
+             if(a[i]<a[j]<a[k])
+             {
+             
+            if(a[i]+a[j]==a[k])
+            {
+                
+                printf("%d %d %d\n",a[i],a[j],a[k]);
+            }
+             
+             
+             
+         }
+         
+         
+         
+         
+         
+         }
+         
+         
+               
+           }
+   }
+   
+    return 0;
+}
