f3095a0953007d73a474b6704fae0d92775b68ff
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-03-14 05:53:00 +0530

    pl108.c

---
+++ pl108.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int N,a[10000],s=0,i,j;
+   scanf("%d",&N);
+   for(i=0;i<N;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<N;i++)
+   {
+       
+       
+  
+    s=s+a[i];
+    printf("%d ",s);
+    
+
+}
+
+}
