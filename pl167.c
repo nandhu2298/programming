84c3a82554cf4635a3aaff1a0f80a9e56b90d1ec
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-03-25 03:08:14 +0530

    pl167.c

---
+++ pl167.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main(void) 
+{
+	char s[30];
+	int i,l,flag=0;
+gets(s);
+	l=strlen(s);
+	for(i=2;i<=l/2;i++)
+	{
+		if(l%i==0)
+		{
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+	{
+		printf("yes");
+		
+	}
+	else
+	{
+		printf("no");
+	}
+	
+	return 0;
+}
