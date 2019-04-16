f1b4c46bb86d4807803b2ad0d4507c0f06b5d3b5
nandhu2298 <46668199+nandhu2298@users.noreply.github.com>
2019-04-16 20:11:29 +0530

    hu119.c

---
+++ hu119.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{ 
+    
+    
+    
+    int N,a[100],i,j,c=0;
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        
+        scanf("%d",&a[i]);
+    }
+  
+  for(i=0;i<N;i++)
+  {
+      
+      
+      for(j=i+1;j<N;j++)
+      {
+          if(i<j&&a[i]<a[j])
+          {
+             
+             c++; 
+          }
+          
+          
+          
+      }
+  }
+  printf("%d",c);
+  
+}
